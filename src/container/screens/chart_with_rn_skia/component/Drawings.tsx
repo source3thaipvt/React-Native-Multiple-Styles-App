f985d40c0d96e4b3540d29d51f04ffb5ebe14476
thaipvt <thaipvt@d2t.vn>
2022-07-26 10:08:20 +0700

    drawings còn dang dở https://www.youtube.com/watch?v=EHxEX78alZE

--- src/container/screens/chart_with_rn_skia/component/Drawings.tsx
+++ src/container/screens/chart_with_rn_skia/component/Drawings.tsx
@@ -1,19 +1,35 @@
-import { StyleSheet, Text, View, useWindowDimensions } from 'react-native'
+import { StyleSheet, View, useWindowDimensions } from 'react-native'
 import React from 'react'
 import HeaderApp from '../../../component/Header';
 import { StylesApp } from '../../../../common/Constant';
 
-import { Canvas, Circle, Group, Oval, rect, center } from '@shopify/react-native-skia';
+import { Canvas, Circle, Group, Oval, rect, center, rrect, Paint, SweepGradient, vec, RadialGradient, BlurMask, Text, DiscretePathEffect, Path } from '@shopify/react-native-skia';
+import images from '../../../../res/images';
 const Drawings = () => {
     const { width, height } = useWindowDimensions();
-    console.log(width, height);
-
+    const rct = { x: width / 6, y: height / 2.22, width: width * 0.7, height: height * 0.1 }
+    const rcenter = { x: width / 2, y: height / 2 }
+    const logo = images.ic_back_black;
     return (
         <Canvas style={{ flex: 1 }}>
-            <Oval rect={{ x: 64, y: 64, width: 100, height: 200 }} color='lightblue' strokeWidth={10} style='stroke' x={width / 2} y={height / 2} />
-            <Circle c={{ x: width / 2, y: height / 2 }} r={20} color='red' />
-            <Group transform={[{ rotate: Math.PI / 3 }]} origin={{ x: width / 2, y: height / 2 }}  >
-                <Oval rect={{ x: 64, y: 64, width: 100, height: 200 }} color='lightblue' strokeWidth={10} style='stroke' x={width / 2} y={height / 2} />
+            <Paint style="stroke" strokeWidth={18}>
+                <RadialGradient c={vec(rcenter.x + 25, rcenter.y)} r={50} colors={['#ff89', '#00fe', '#ff0000']} />
+            </Paint>
+
+            <Circle c={rcenter} r={10} color='red' />
+            <Paint style="stroke" strokeWidth={18}>
+                <SweepGradient c={rcenter} colors={['#ff89', '#00fe', '#ff0000']} />
+                <BlurMask blur={20} style='normal' />
+                <DiscretePathEffect deviation={5} length={10} />
+            </Paint>
+            <Group>
+                <Oval rect={rct} color='lightblue' strokeWidth={10} style='stroke' />
+                <Group transform={[{ rotate: Math.PI / 3 }]} origin={{ x: width / 2, y: height / 2 }} >
+                    <Oval rect={rct} color='lightblue' strokeWidth={10} style='stroke' />
+                </Group>
+                <Group transform={[{ rotate: -Math.PI / 3 }]} origin={{ x: width / 2, y: height / 2 }}  >
+                    <Oval rect={rct} color='lightblue' strokeWidth={10} style='stroke' />
+                </Group>
             </Group>
         </Canvas>
     )
